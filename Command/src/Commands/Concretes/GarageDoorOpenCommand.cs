03a552bbd5ff2a38427859f33a76be3a5446f634
Axel Javier Ayala Siles <axel.ayala@fundacion-jala.org>
2024-08-12 23:00:48 -0400

    refactor: concretes command for TV and Garage door implemented

---
+++ Command/src/Commands/Concretes/GarageDoorOpenCommand.cs
@@ -0,0 +1,25 @@
+using Command.src.Commands.Interface;
+using Command.src.Receivers;
+
+namespace Command.src.Commands.Concretes;
+
+public class GarageDoorOpenCommand : ICommand
+{
+    private readonly GarageDoor _garageDoor;
+
+    public GarageDoorOpenCommand(GarageDoor garageDoor)
+    {
+        _garageDoor = garageDoor;
+    }
+
+    public void Execute()
+    {
+        _garageDoor.Open();
+    }
+
+    public void Undo()
+    {
+        _garageDoor.Close();
+    }
+
+}
