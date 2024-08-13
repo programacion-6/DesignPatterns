0efb68c32fe916291b03150dd3a3dcfa5bc94d7b
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-12 20:05:52 -0400

    refactor: move Light classes to the corresponding folders; separate into files

---
+++ Command/Commands/LightCommands/LightOffCommand.cs
@@ -0,0 +1,24 @@
+using Command.Commands.Interfaces;
+using Command.Receivers;
+
+namespace Command.Commands.LightCommands;
+
+public class LightOffCommand : ICommand
+{
+    private readonly Light _light;
+
+    public LightOffCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {
+        _light.TurnOff();
+    }
+
+    public void Undo()
+    {
+        _light.TurnOn();
+    }
+}
---
+++ Command/Receivers/Light.cs
@@ -0,0 +1,14 @@
+namespace Command.Receivers;
+
+public class Light
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("The light is on");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("The light is off");
+    }
+}
