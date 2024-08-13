f5cae16eb017bf634bdf2ee024d0f424bb2ae2b1
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-08-12 22:10:12 -0400

    refactor: move entities to the models folder; implementation of garage door and fan entities

---
+++ Command/Models/Fan.cs
@@ -0,0 +1,14 @@
+namespace Command;
+
+public class Fan
+{
+    public void On()
+    {
+        Console.WriteLine("The fan is on");
+    }
+
+    public void Off()
+    {
+        Console.WriteLine("The fan is off");
+    }
+}
